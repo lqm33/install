d4dcd180f908d5511a5697d1d30112c794c45f7c
Lqm33 <51096589+lqm33@users.noreply.github.com>
2020-10-09 00:42:13 +0300

    Add files via upload

---
+++ pac.sh
@@ -0,0 +1,10 @@
+sudo pacman -S telegram-desktop
+yay -S burpsuite
+yay -S nmap
+yay -S pulseaudio
+sudo pacman -S snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install walc
+yay -S flameshot
+snap install spotify
